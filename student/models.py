5dc6fa9906f7816727368f3448ab07688fe8d53f
zmm <1614180462@qq.com>
2017-03-31 21:52:37 +0800

    添加用户模型

--- student/models.py
+++ student/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    #账号
+    stu_num = models.CharField(max_length=30,verbose_name='用户账号')
+    #密码
+    stu_password = models.CharField(max_length=20,verbose_name='用户密码')
+    #邮箱
+    stu_email = models.EmailField(verbose_name='邮箱地址')
