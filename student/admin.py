370059b36b555f0dd9d68bf233491368a1d28b63
zmm <1614180462@qq.com>
2017-04-03 22:11:09 +0800

    admin修改

--- student/admin.py
+++ student/admin.py
@@ -3,4 +3,8 @@ from django.contrib.admin import AdminSite
 from .models import *
 # Register your models here.
 
-admin.site.register(User)
+AdminSite.site_header="学生选课管理系统"
+AdminSite.site_title = "学生选课管理系统"
+
+admin.site.register(Student)
+admin.site.register(Course)
